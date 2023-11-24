9fbac877ac4ae7c8b6cd047773bef851630e5e29
MiriamLaw <mimsical999@yahoo.com>
2023-11-24 14:06:36 -0800

    Ready for submission, will test more

--- src/test/java/com/coderscampus/assignment9/Assignment9ApplicationTests.java
+++ src/test/java/com/coderscampus/assignment9/Assignment9ApplicationTests.java
@@ -70,16 +70,75 @@ class Assignment9ApplicationTests {
 	
 	@Test
 	void testGetVeganRecipes() throws Exception {
+		List<Recipe> allRecipes = new ArrayList<>();
 		
+		Recipe veganRecipe = new Recipe(45, true, true, "1. Place the lentils, turmeric, salt and water in a large saucepan", 
+				15.0, 59.89, 60, 6, 63.0, "Zucchini with Lentils and Roasted Garlic", true, true);
+		
+		allRecipes.add(veganRecipe);
+		
+		Recipe nonVeganRecipe = new Recipe(135, false, false, 
+				"1 Heat oven to 350F. Grease bottom and sides of 13x9-inch pan with shortening", 
+				30.0, 99.92, 165, 12, 54.0, "Butterfly Stencil Cake", false, false);
+		
+		allRecipes.add(nonVeganRecipe);
+		
+		Mockito.when(recipeRepository.getAllRecipes()).thenReturn(allRecipes);
+		List<Recipe> veganRecipes = recipeService.getVeganRecipes();
+		
+		for (Recipe recipe : veganRecipes) {
+			assertTrue(recipe.getVegan());
+		}
 	}
 	
 	@Test
 	void testVeganAndGlutenFreeRecipes() throws Exception {
+		List<Recipe> allRecipes = new ArrayList<>();
+		
+		Recipe veganAndGlutenFreeRecipe = new Recipe(40, true, true, 
+				"To Make The Almond Topping:Stir all ingredients together until combined", 
+				5.0, 118.77, 45, 8, 39.0, "Berry Almond Crisp", true, true);
+		
+		allRecipes.add(veganAndGlutenFreeRecipe);
+		
+		Recipe nonVeganAndGlutenFreeRecipe = new Recipe(0, false, false, "Procedures 1 Make the crust", 
+				0.0, 131.78, 75, 8, 34.0, "Meyer Lemon Mascarpone Cake", false, false);
+				
+				allRecipes.add(nonVeganAndGlutenFreeRecipe);
+		
+		Mockito.when(recipeRepository.getAllRecipes()).thenReturn(allRecipes);
+		
+		List<Recipe> veganAndGlutenFreeRecipes = recipeService.getVeganAndGlutenFreeRecipes();
+		
+		for (Recipe recipe : veganAndGlutenFreeRecipes) {
+			assertTrue(recipe.getVegan());
+			assertTrue(recipe.getGlutenFree());
+		}
+		
 		
 	}
 	
 	@Test
 	void testVegetarianRecipes() throws Exception {
+		List<Recipe> allRecipes = new ArrayList<>();
+		
+		Recipe vegetarianRecipe = new Recipe(45, true, true, "1. Place the lentils, turmeric, salt and water in a large saucepan", 
+				15.0, 59.89, 60, 6, 63.0, "Zucchini with Lentils and Roasted Garlic", true, true);
+		
+		allRecipes.add(vegetarianRecipe);
+		
+		Recipe nonVegetarianRecipe = new Recipe(135, false, false, 
+				"1 Heat oven to 350F. Grease bottom and sides of 13x9-inch pan with shortening", 
+				30.0, 99.92, 165, 12, 54.0, "Butterfly Stencil Cake", false, false);
+		
+		allRecipes.add(nonVegetarianRecipe);
+		
+		Mockito.when(recipeRepository.getAllRecipes()).thenReturn(allRecipes);
+		List<Recipe> vegetarianRecipes = recipeService.getVegetarianRecipes();
+		
+		for (Recipe recipe : vegetarianRecipes) {
+			assertTrue(recipe.getVegetarian());
+		}
 		
 	}
 
